73157d6b46d98e200da8dd974af67732acf1c343
Kamil Jarzab <bogurodzic@gmail.com>
2017-06-25 15:15:17 +0200

    Screen is now displaying current number

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,28 @@
 window.onload = function(){
+  let screen = {
+    currentNumber: [],
+    display: document.getElementById("display"),
+
+    displayCurrentNumber: function(){
+      this.displayNumber(this.concatNumbers());
+    },
+
+    concatNumbers: function(){
+      let screen = Number(this.currentNumber.join(""));
+        return screen;
+    },
+
+    displayNumber: function(num){
+      this.display.innerText = num;
+    }
+  }
 
   //Events for displaying buttons number
   let numbers = document.getElementsByClassName("number");
   [].forEach.call(numbers, function(data, index){
     data.addEventListener("click", function(){
-      console.log(Number(this.innerText));
+      screen.currentNumber.push(Number(this.innerText));
+      screen.displayCurrentNumber();
     });
   });
 }
